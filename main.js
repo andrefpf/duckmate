c6d21adb1fff8a9b4933bb49bbb6a817dbb2cda4
André Fernandes <fpf.andre@gmail.com>
2022-12-06 19:21:28 -0300

    Update board dinamically

--- main.js
+++ main.js
@@ -1,26 +1,61 @@
 class Piece {
     constructor(text) {
-        this.text = "♕";
+        this.text = text;
     }
 }
 
 class Board {
     constructor() {
         this.data = Array(8*8).fill(null);
+        this.rearrange();
     }
 
     rearrange() {
-        for (i=8; i<16; i++) {
-            this.data[8 + i] = Piece("♟︎");
+        this.data[0] = new Piece("♖")
+        this.data[7] = new Piece("♖")
+        this.data[1] = new Piece("♘")
+        this.data[6] = new Piece("♘")
+        this.data[2] = new Piece("♗")
+        this.data[5] = new Piece("♗")
+        this.data[3] = new Piece("♕")
+        this.data[4] = new Piece("♔")
+        
+        for (let i=8; i<16; i++) {
+            this.data[i] = new Piece("♙");
         }
 
-        for (i=64-8; i<64; i++) {
-            this.data[i] = Piece("♙");
+        this.data[8*4 + 4] = new Piece("🐥")
+        
+        this.data[56 + 0] = new Piece("♜")
+        this.data[56 + 7] = new Piece("♜")
+        this.data[56 + 1] = new Piece("♞")
+        this.data[56 + 6] = new Piece("♞")
+        this.data[56 + 2] = new Piece("♝")
+        this.data[56 + 5] = new Piece("♝")
+        this.data[56 + 3] = new Piece("♛")
+        this.data[56 + 4] = new Piece("♚")
+
+        for (let i=(40+8); i<(40+16); i++) {
+            this.data[i] = new Piece("♟︎");
         }
     }
 }
 
 function update_interface(board) {
     var table = document.getElementById("chess-board");
-    console.log(table.rows[0].cols[0])
+
+    for (let i=0; i<8; i++) {
+        for (let j=0; j<8; j++) {
+            let square =  board.data[i*8 + j];
+            if (square !== null) {
+                console.log(square);
+                table.rows[7-i].cells[j+1].innerText = square.text;
+            }
+        }
+    }
+}
+
+function main() {
+    let b = new Board()
+    update_interface(b)
 }
