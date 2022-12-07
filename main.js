4318bb6f7de61879e08a0f9dcaeddc3ce63a350c
André Fernandes <fpf.andre@gmail.com>
2022-12-06 22:09:41 -0300

    Enable pieces drag

--- main.js
+++ main.js
@@ -41,7 +41,10 @@ class Board {
     }
 }
 
-function update_interface(board) {
+let x = 0;
+let y = 0;
+
+function updateInterface(board) {
     var table = document.getElementById("chess-board");
 
     if (table === null) {
@@ -58,7 +61,54 @@ function update_interface(board) {
     }
 }
 
+const mouseDownHandler = function (e) {
+    draggablePiece = document.getElementById("draggable-piece");
+    draggablePiece.innerText = e.target.innerText;
+    
+    // Calculate the mouse position
+    const rect = e.target.getBoundingClientRect();
+    x = e.pageX - rect.left;
+    y = e.pageY - rect.top;
+
+    draggablePiece.style.top = `${e.pageY - y}px`;
+    draggablePiece.style.left = `${e.pageX - x}px`;
+
+    // Attach the listeners to `document`
+    document.addEventListener("mousemove", mouseMoveHandler);
+    document.addEventListener("mouseup", mouseUpHandler);
+};
+
+const mouseMoveHandler = function (e) {
+    console.log(e.target.innerText);
+
+    // Set position for dragging element
+    draggablePiece.style.top = `${e.pageY - y}px`;
+    draggablePiece.style.left = `${e.pageX - x}px`;
+};
+
+const mouseUpHandler = function () {
+    // Remove the position styles
+    draggablePiece.style.removeProperty("top");
+    draggablePiece.style.removeProperty("left");
+    draggablePiece.innerText = "";
+
+    x = null;
+    y = null;
+    draggablePiece = null;
+
+    // Remove the handlers of `mousemove` and `mouseup`
+    document.removeEventListener("mousemove", mouseMoveHandler);    
+    document.removeEventListener("mouseup", mouseUpHandler);
+};
+
 window.onload = function main() {
+    var table = document.getElementById("chess-board");
+
+    [].slice.call(table.querySelectorAll(".dark, .light")).forEach(function (item) {
+        item.addEventListener("mousedown", mouseDownHandler);
+    });
+
+
     let b = new Board()
-    update_interface(b)
+    updateInterface(b)
 }
